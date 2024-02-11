2acf1e233613f6e646aaff23db195e3cec6c9227
Bryan Sample <bryanjsample@gmail.com>
2024-02-11 15:48:37 -0600

    More SQL statements

--- all_nhl_player_season/sql_scripts/examples.sql
+++ all_nhl_player_season/sql_scripts/examples.sql
@@ -1,12 +1,34 @@
 USE nhlStats
 
+
+-- SELECT * FROM players;
+
+-- SELECT * FROM seasons;
+
+-- SELECT * FROM teams;
+
+-- SELECT * FROM seasonInfo;
+
+-- SELECT * FROM scoring;
+
+-- SELECT * FROM goals;
+
+-- SELECT * FROM assists;
+
+-- SELECT * FROM shots;
+
+-- SELECT * FROM miscStats;
+
+-- SELECT * FROM goalieStats;
+
 -- SELECT p.firstName as FirstName,
 -- p.lastName as LastName,
 -- p.birthLocationPrimary as City,
 -- p.birthLocationSecondary as State,
 -- p.birthCountry as Country
 -- FROM players p
--- WHERE p.birthLocationSecondary IN ('Minnesota', 'Colorado');
+-- WHERE p.birthLocationSecondary IN ('Minnesota', 'Colorado')
+-- ORDER BY p.birthLocationSecondary, p.birthLocationPrimary;
 
 -- SELECT 
 -- p.playerId as id,
@@ -20,5 +42,26 @@ USE nhlStats
 -- INNER JOIN
 -- scoring s
 -- ON p.playerId = s.playerId
--- WHERE p.lastName = 'Gretzky'
+;
+
+-- WHERE p.playerId = 'nichuva01'
+
+-- SELECT 
+-- p.playerId as id,
+-- p.firstName as FirstName,
+-- p.lastName as LastName,
+-- p.pos as Position,
+-- m.seasonId as Season,
+-- m.penaltyMinutes as PIM,
+-- m.timeOnIce as TOI,
+-- m.faceoffWins as FOW,
+-- m.faceoffLosses as FOL,
+-- m.shotsBlocked as BlockedShots,
+-- m.hits as Hits,
+-- m.takeaways as Takeaways,
+-- m.giveaways as Giveaways
+-- FROM players p
+-- INNER JOIN
+-- miscStats m
+-- ON p.playerId = m.playerId
 -- ;
